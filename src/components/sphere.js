ada3bbaa71657cc441bf14fdd48cc6dc5ae98f88
Sanidhya <sharmasanidhya0504@gmail.com>
2022-05-29 22:16:26 +0530

    first commit

---
+++ src/components/sphere.js
@@ -0,0 +1,56 @@
+import "../styles/sphere.css";
+import React, { useEffect } from "react";
+import { extend } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+extend({ OrbitControls });
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+{
+  /*}
+const Controls = () => {
+  const orbitRef = useRef();
+  const { camera, gl } = useThree();
+  useFrame(() => {
+    orbitRef.current.update();
+  });
+
+  return (
+    <OrbitControls
+      maxPolarAngle={Math.PI / 3}
+      minPolarAngle={Math.PI / 3}
+      args={[camera, gl.domElement]}
+      ref={orbitRef}
+      enableZoom={false}
+    />
+  );
+};
+*/
+}
+
+const Sphere = ({ args, color }) => {
+  return (
+    <points>
+      <sphereGeometry args={args} />
+      <pointsMaterial size={0.02} color={color} sizeAttenuation />
+    </points>
+  );
+};
+
+export default function SphereComp() {
+  return (
+    <div className="sphere_wrap">
+      <Canvas>
+        <perspectiveCamera position={(0, 0, 0)}>
+          <Sphere args={[5, 120, 32]} color={0x0000ff} />
+          <Sphere args={[4, 60, 32]} color={0xf2f4f8} />
+        </perspectiveCamera>
+        <OrbitControls autoRotate={true} enablePan={false} enableZoom={false} />
+      </Canvas>
+    </div>
+  );
+}
